0445ece52f3182725d27fa769abe108f69464204
Henrique Cassús <henriquecassus@gmail.com>
2016-02-26 10:44:38 -0300

    Handling behave's behaviour

--- PageObjects/MainPage.py
+++ PageObjects/MainPage.py
@@ -10,8 +10,8 @@ class MainPage(GenericPage):
     locator_avatar = (By.CLASS_NAME, 'foto-do-usuario')
 
     def obtain_user(self):
-        avatar_element = self.driver.find_element(*self.locator_avatar)
+        avatar_element = self.driver.find_element(*MainPage.locator_avatar)
         avatar_element.click()
 
-        user_element = self.driver.find_element(*self.locator_logged_user)
+        user_element = self.driver.find_element(*MainPage.locator_logged_user)
         return user_element.text
